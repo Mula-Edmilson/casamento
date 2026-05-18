7c2995324d4ad78e20dffb94cd9e3b7c7e65137b
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-05-18 13:48:05 +0200

    Add files via upload

--- convite/rosalina-monteiro/client-config.js
+++ convite/rosalina-monteiro/client-config.js
@@ -1,3 +1,4 @@
-window.LIRANDZO_INVITE_SLUG = "rosalina-monteiro";
-window.LIRANDZO_API_BASE_URL = "https://api-casamento-mj.onrender.com";
+window.LIRANDZO_INVITE_SLUG = 'rosalina-monteiro';
+window.LIRANDZO_API_BASE_URL = 'https://api-casamento-mj.onrender.com';
 window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, '') + '/api';
+window.LIRANDZO_CHECKIN_PASSWORD = window.LIRANDZO_CHECKIN_PASSWORD || 'checkin2026';
