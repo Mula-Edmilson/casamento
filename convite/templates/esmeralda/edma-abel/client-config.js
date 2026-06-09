4d4afc9014ac26b675b6a528d2fdf124fcd27316
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-06-10 00:02:08 +0200

    Add files via upload

---
+++ convite/templates/esmeralda/edma-abel/client-config.js
@@ -0,0 +1,5 @@
+// client-config.js — Convite Edma & Abel
+window.LIRANDZO_INVITE_SLUG = 'edma-abel';
+window.LIRANDZO_API_BASE_URL = 'https://api-casamento-mj.onrender.com';
+window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, '') + '/api';
+window.LIRANDZO_CHECKIN_PASSWORD = window.LIRANDZO_CHECKIN_PASSWORD || 'checkin2026';
