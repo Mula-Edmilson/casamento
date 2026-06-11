b2f0ca45088f9399c484913193e05d80179a4ccc
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-06-11 10:13:52 +0200

    Delete convite/templates/perola/minoca-abubacar directory

--- convite/templates/perola/minoca-abubacar/client-config.js
+++
@@ -1,3 +0,0 @@
-window.LIRANDZO_INVITE_SLUG = "minoca-abubacar";
-window.LIRANDZO_API_BASE_URL = window.location.origin;
-window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$, "") + "/api";
