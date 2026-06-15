de0b58af130413f3bffe8fb7451abdeb1e161d02
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-06-15 12:02:09 +0200

    Add files via upload

--- convite/minoca-abubacar/client-config.js
+++ convite/minoca-abubacar/client-config.js
@@ -1,3 +1,3 @@
 window.LIRANDZO_INVITE_SLUG = "minoca-abubacar";
-window.LIRANDZO_API_BASE_URL = window.location.origin;
-window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$, "") + "/api";
+window.LIRANDZO_API_BASE_URL = "https://api-casamento-mj.onrender.com";
+window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, "") + "/api";
