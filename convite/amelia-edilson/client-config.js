4a73e1def5deee818978c7934196737e39e93edf
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-06-24 17:46:09 +0200

    Add files via upload

--- convite/amelia-edilson/client-config.js
+++ convite/amelia-edilson/client-config.js
@@ -1,3 +1,6 @@
-window.LIRANDZO_INVITE_SLUG = "amelia-edilson";
-window.LIRANDZO_API_BASE_URL = "https://api-casamento-mj.onrender.com";
+window.LIRANDZO_INVITE_SLUG = 'amelia-edilson-convite';
+window.LIRANDZO_API_BASE_URL = 'https://api-casamento-mj.onrender.com';
 window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, '') + '/api';
+// Compatibilidade com o motor visual deste convite.
+window.CONVITE_SLUG = window.LIRANDZO_INVITE_SLUG;
+window.CONVITE_API_URL = window.LIRANDZO_API_BASE_URL;
