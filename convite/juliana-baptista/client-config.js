9b46fdaa4c5686dc7210dd1f42ec7bc7f2e257c8
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-08-02 11:54:26 +0200

    Criar convite juliana-baptista (Pérola)

---
+++ convite/juliana-baptista/client-config.js
@@ -0,0 +1,3 @@
+window.LIRANDZO_INVITE_SLUG = "juliana-baptista";
+window.LIRANDZO_API_BASE_URL = "https://api-casamento-mj.onrender.com";
+window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, '') + '/api';
