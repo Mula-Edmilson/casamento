b071dff94cbecbe7c0bff4347f22fc1d64734aac
Mula-Edmilson <mula.edmilson0@gmail.com>
2026-09-10 20:40:35 +0200

    Delete convite/edna-mauro/edma-abel directory

--- convite/edna-mauro/edma-abel/client-config.js
+++
@@ -1,5 +0,0 @@
-// client-config.js — Convite Edma & Abel
-window.LIRANDZO_INVITE_SLUG = 'edma-abel';
-window.LIRANDZO_API_BASE_URL = 'https://api-casamento-mj.onrender.com';
-window.LIRANDZO_API_URL = window.LIRANDZO_API_BASE_URL.replace(/\/+$/, '') + '/api';
-window.LIRANDZO_CHECKIN_PASSWORD = window.LIRANDZO_CHECKIN_PASSWORD || 'checkin2026';
